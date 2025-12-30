af4ed2c77cbbfb4088193a525c5da1cd75fb4bd2
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2025-12-30 16:05:39 +0530

    fix(components): import syntax to use the registered bhailang syntax

--- app/src/Components/Documentation.tsx
+++ app/src/Components/Documentation.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-// import { bhaiLangSyntax } from "../syntax";
+import "../syntax";
 import Prism from "prismjs";
 import "prismjs/themes/prism-tomorrow.css"; // dark theme
 import { documentation } from "../data/documentation";

083802c755a584e609b18b8ccc0d7097ab85b4b3
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2025-12-30 12:29:34 +0530

    feat(components): accept output and errors arrays to display neatly
    - Errors are printed in red
    - Standard output is printed normally

--- app/src/Components/TerminalOutput.tsx
+++ app/src/Components/TerminalOutput.tsx
@@ -2,16 +2,26 @@ import React from "react";
 
 interface TerminalOutputProps {
   output: string[];
+  errors?: string[];
 }
 
-const TerminalOutput: React.FC<TerminalOutputProps> = ({ output }) => {
+const TerminalOutput: React.FC<TerminalOutputProps> = ({
+  output,
+  errors = [],
+}) => {
   return (
     <div className="flex justify-center mt-4">
       <div className="bg-black text-white py-2 px-4 min-w-[70vw] max-w-2xl rounded-md font-mono overflow-auto">
-        {output}
-        {output.map((o) => {
-          return <div key={o}>{o}</div>;
-        })}
+        {output.map((line, i) => (
+          <div key={`out-${i}`} className="text-white">
+            {line}
+          </div>
+        ))}
+        {errors.map((err, i) => (
+          <div key={`err-${i}`} className="text-red-500">
+            {err}
+          </div>
+        ))}
       </div>
     </div>
   );

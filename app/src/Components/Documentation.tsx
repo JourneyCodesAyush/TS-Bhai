de953919a1d1e2a8fe019e4497fca287ecfba8c6
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2025-12-30 15:54:18 +0530

    refactor(components): remove unused imports

--- app/src/Components/Documentation.tsx
+++ app/src/Components/Documentation.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { bhaiLangSyntax } from "../syntax";
+// import { bhaiLangSyntax } from "../syntax";
 import Prism from "prismjs";
 import "prismjs/themes/prism-tomorrow.css"; // dark theme
 import { documentation } from "../data/documentation";
